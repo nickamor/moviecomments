28aaa5e11aa613ac3179f63281c71feabb51c547
Nicholas Amor <nick.amor@gmail.com>
2016-10-13 13:26:49 +1100

    Add routes for authentication

--- blogs/routes/index.js
+++ blogs/routes/index.js
@@ -1,7 +1,22 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var credentials = require('../config/credentials');
 
 const blogController = require('../controllers/blog_controller.js');
+
+// Prevent database connection errors for long-running applications.
+var opts = {
+	server: {
+		socketOptions: {
+			keepAlive: 1
+		}
+	}
+};
+
+mongoose.connect(credentials.mongo.devel.connection, opts);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,4 +33,42 @@ router.post('/movie/:movieId/comments', function (req, res, next) {
   blogController.newComment(req, res, next);
 });
 
+/** auth routes **/
+
+router.get('/login', function(req, res, next) {
+	res.render('login', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res, next) {
+	res.render('signup', { message: req.flash('signupMessage') });
+});
+
+router.get('/profile', requireAuth, function(req, res) {
+	res.render('profile', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+	successRedirect: '/profile',
+	failureRedirect: '/signup',
+	failureFlash: true
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+	successRedirect: '/profile',
+	failureRedirect: '/login',
+	failureFlash: true
+}));
+
 module.exports = router;
+
+function requireAuth(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/');
+}
